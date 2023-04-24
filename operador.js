316c3f93651000a33285579a6774deace50e94b5
Liovergara <129076556+Liovergara@users.noreply.github.com>
2023-04-24 09:21:42 -0500

    main

--- operador.js
+++ operador.js
@@ -1,1 +1,7 @@
-var celsius = 
+//conversion larega:
+var celsius1 = prompt("Introduzca la temperatura en grados celsius:");
+celsius1 = parseFloat(celsius);
+
+
+//Conversion larga:
+//var celius1 = parseFloat(prompt ("Introduzca la temperatura en grados celsius"))
